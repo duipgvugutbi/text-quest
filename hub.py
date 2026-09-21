c051daca955f4f09a77589b9f04715d8e38a1b0d
Kopatich <cooldown201145453@gmail.com>
2026-09-21 18:36:23 +0300

    hub.py

--- hub.py
+++ hub.py
@@ -75,7 +75,7 @@ def hub():
     elif gs.answer == "5":
         print("Вы пришли к озеру")
         location5()
-    elif gs.answer == "6":
+    elif gs.answer == "6":ги
         print("Вы подошли к скупщику")
         trader()
     elif gs.answer == "7":
@@ -86,3 +86,4 @@ def hub():
     else:
         print("Неверный ввод, попробуйте снова")
         hub()
+hub()
